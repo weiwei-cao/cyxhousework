06d56798e37c2d48ab66275cb50eb769ea3d34d9
weiwei-cao <1323791468@qq.com>
2020-06-17 09:05:00 +0800

    曹永霞作业第一次项目结构上传

--- src/day03/HomeWork.java
+++ src/day03/HomeWork.java
@@ -3,6 +3,13 @@ package day03;
 public class HomeWork {
     //1.打印99乘法表2.打印等腰三角形
     public static void main(String[] args) {
+        int i;
+        int j;
+        for(i=1;i<10;i++){
+            for(j=1;j<10;j++){
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+        }
         System.out.println("打印成功");
     }
 }
