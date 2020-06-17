44caa5d30a2efd1e51b35a4fa5aee046fc043827
weiwei-cao <1323791468@qq.com>
2020-06-17 11:26:46 +0800

    曹永霞作业最终版项目结构上传

--- src/day03/HomeWork.java
+++ src/day03/HomeWork.java
@@ -5,11 +5,22 @@ public class HomeWork {
     public static void main(String[] args) {
         int i;
         int j;
+        int s;
         for(i=1;i<10;i++){
             for(j=1;j<10;j++){
-                System.out.println(i+"*"+j+"="+i*j);
+                System.out.print(i+"*"+j+"="+i*j+"   ");
             }
+            System.out.println();
         }
         System.out.println("打印成功");
+        for(i=1;i<9;i++){
+            for(j=1;j<=9-i;j++){
+                System.out.print(" ");
+            }
+            for(s=1;s<=2*i-1;s++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
