d6f7b0b3ed145df65fbc5c61952bc2c3d7825d04
weiwei-cao <1323791468@qq.com>
2020-06-17 16:19:37 +0800

    曹永霞下午改题后作业最终版项目结构上传

--- src/day03/HomeWork.java
+++ src/day03/HomeWork.java
@@ -1,26 +1,33 @@
 package day03;
 
 public class HomeWork {
-    //1.打印99乘法表2.打印等腰三角形
+    //1.打印等腰三角形
+    //2.求100以内所有质数的和
     public static void main(String[] args) {
-        int i;
-        int j;
-        int s;
-        for(i=1;i<10;i++){
-            for(j=1;j<10;j++){
-                System.out.print(i+"*"+j+"="+i*j+"   ");
-            }
-            System.out.println();
-        }
-        System.out.println("打印成功");
-        for(i=1;i<9;i++){
-            for(j=1;j<=9-i;j++){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4-i;j++){
                 System.out.print(" ");
             }
-            for(s=1;s<=2*i-1;s++){
+            for(int k=0;k<2*i+1;k++){
                 System.out.print("*");
             }
             System.out.println();
         }
+        int s=0;
+
+        for(int i=2;i<100;i++){
+            boolean real=true;
+            for(int j=2;j<i;j++){
+                if(i%j==0){
+                    real=false;
+                    break;
+                }
+            }
+            if(real){
+                s=s+i;
+                System.out.println(i);
+            }
+        }
+        System.out.println(s);
     }
 }
